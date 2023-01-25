df5131cb52ab6b470d91223b60d27a38cb16d353
Fixel <fixelti@gmail.com>
2023-01-25 18:57:23 +0300

    main structure of the handler

--- internal/transport/rest/pathBuilding/handler.go
+++ internal/transport/rest/pathBuilding/handler.go
@@ -1,1 +1,24 @@
 package pathBuilding
+
+import (
+	"navigation/internal/logging"
+	"navigation/internal/transport/rest/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+const pathBuildingURL = "/path-building"
+
+type handler struct {
+	logger *logging.Logger
+}
+
+func NewHandler(logger *logging.Logger) handlers.Handler {
+	return &handler{logger: logger}
+}
+
+func (h *handler) Register(router *gin.RouterGroup) {}
+
+func (h *handler) pathBuilding(c *gin.Context) error {
+	return nil
+}
