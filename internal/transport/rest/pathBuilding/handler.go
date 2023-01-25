766fa418e12f584669cbd253a5479d26e511b992
Fixel <fixelti@gmail.com>
2023-01-25 19:13:58 +0300

    create pathBuilding handler

--- internal/transport/rest/pathBuilding/handler.go
+++ internal/transport/rest/pathBuilding/handler.go
@@ -1,8 +1,11 @@
 package pathBuilding
 
 import (
+	"navigation/internal/app/pathBuilder"
+	"navigation/internal/database/client/postgresql"
 	"navigation/internal/logging"
 	"navigation/internal/transport/rest/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +14,7 @@ const pathBuildingURL = "/path-building"
 
 type handler struct {
 	logger *logging.Logger
+	client postgresql.Client
 }
 
 func NewHandler(logger *logging.Logger) handlers.Handler {
@@ -22,6 +26,28 @@ func (h *handler) Register(router *gin.RouterGroup) {
 	pathBuilding.GET("", h.pathBuilding)
 }
 
+type auditorys struct {
+	Start int `form:"start" binding:"required"`
+	End int `form:"end" binding:"required"`
+}
+
 func (h *handler) pathBuilding(c *gin.Context) {
-	return nil
+	var auditorys auditorys
+
+	if err := c.ShouldBindQuery(&auditorys); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "can't decode query"})
+		return
+	}
+
+	pathBuilder := pathBuilder.NewPathBuilder(h.logger, h.client)
+
+	// TODO сделать обработку ошибки
+	res, err := pathBuilder.Builder(auditorys.Start, auditorys.End)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+	return
 }
