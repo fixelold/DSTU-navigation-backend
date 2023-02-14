0af7d639862ffe884f4fd577391ad66da7912eae
Fixel <fixelti@gmail.com>
2023-02-14 21:12:20 +0300

    create drawAxisX

--- internal/app/drawPath/drawPathAuditory.go
+++ internal/app/drawPath/drawPathAuditory.go
@@ -8,21 +8,41 @@ import (
 const (
 	AxisX = 1
 	AxisY = 2
+
+	WidhtX = 130
+	HeightX = 30
+
+	WidhtY = 30
+	HeightY = 130
+	
+	plus = 0
+	minus = 1
 )
 
 var (
 	User000004 = appError.NewError("drawPath", "GetSelector", "Input does not match desired length", "-", "US-000004")
 )
 
-func DrawPathAuditory(position, auditory *models.Reactangle) ([]int, error) {
+func DrawPathAuditory(borderPoints, auditory *models.Reactangle) ([]int, error) {
+	var path models.Reactangle
 	var err error
 	var points []int
-	axis := defenitionAxis(position)
+	axis := defenitionAxis(borderPoints)
 
 	switch axis {
 
 	case AxisX:
-		//....
+		path.X = borderPoints.Widht / 2
+		path.Y = borderPoints.Y
+		path.Widht = path.X + WidhtX
+		path.Height = path.Y + HeightX
+
+		if checkBorder(&path, auditory) {
+			points = append(points, path.X, path.Y, path.Widht, path.Height)
+		} else {
+			path.Widht = path.X - WidhtX
+			path.Height = path.Y - HeightX
+		}
 	case AxisY:
 		//....
 	default:
@@ -33,6 +53,30 @@ func DrawPathAuditory(position, auditory *models.Reactangle) ([]int, error) {
 	// 4 возвратить данные
 
 	return points, err
+}
+
+func drawAxisX(borderPoints *models.Reactangle, sign int) (*models.Reactangle, error) {
+	var path models.Reactangle
+	var err error
+	switch sign {
+	
+	case plus:
+		path.X = borderPoints.Widht / 2
+		path.Y = borderPoints.Y
+		path.Widht = path.X + WidhtX
+		path.Height = path.Y + HeightX
+
+	case minus:
+		path.X = borderPoints.Widht / 2
+		path.Y = borderPoints.Y
+		path.Widht = path.X - WidhtX
+		path.Height = path.Y - HeightX
+	
+	default:
+		err = User000004
+	}
+
+	return &path, err
 }
 
 func defenitionAxis(position *models.Reactangle) int {
