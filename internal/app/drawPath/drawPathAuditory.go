8bed8e492b57009b4114f6062cd7a73089cd315a
Fixel <fixelti@gmail.com>
2023-02-15 11:42:24 +0300

    create draw func and finily drawPathAudiotry func

--- internal/app/drawPath/drawPathAuditory.go
+++ internal/app/drawPath/drawPathAuditory.go
@@ -1,6 +1,7 @@
 package drawPath
 
 import (
+	"database/sql/driver"
 	"navigation/internal/appError"
 	"navigation/internal/models"
 )
@@ -24,7 +25,6 @@ var (
 )
 
 func DrawPathAuditory(borderPoints, auditory *models.Reactangle) ([]int, error) {
-	var path models.Reactangle
 	var err error
 	var points []int
 	axis := defenitionAxis(borderPoints)
@@ -32,30 +32,50 @@ func DrawPathAuditory(borderPoints, auditory *models.Reactangle) ([]int, error) 
 	switch axis {
 
 	case AxisX:
-		path.X = borderPoints.Widht / 2
-		path.Y = borderPoints.Y
-		path.Widht = path.X + WidhtX
-		path.Height = path.Y + HeightX
-
-		if checkBorder(&path, auditory) {
-			points = append(points, path.X, path.Y, path.Widht, path.Height)
-		} else {
-			path.Widht = path.X - WidhtX
-			path.Height = path.Y - HeightX
+		path, err := draw(WidhtX, HeightX, borderPoints, auditory)
+		if err != nil {
+			return nil, err
 		}
+
+		points = append(points, path.X, path.Y, path.Widht, path.Height)
 	case AxisY:
-		//....
+		path, err := draw(WidhtY, HeightY, borderPoints, auditory)
+		if err != nil {
+			return nil, err
+		}
+
+		points = append(points, path.X, path.Y, path.Widht, path.Height)
 	default:
 		err = User000004
 	}
-	// 3 начать рисовать
-
-	// 4 возвратить данные
 
 	return points, err
 }
 
-func drawAxisX(borderPoints *models.Reactangle, sign int) (*models.Reactangle, error) {
+func draw(widht, height int, borderPoints, auditory *models.Reactangle) (models.Reactangle, error) {
+	var err error
+	path, err := drawAxisX(widht, height, borderPoints, plus)
+	if err != nil {
+		return path, err
+	}
+
+	if checkBorder(&path, auditory) {
+		return path, nil
+	} else {
+		path, err = drawAxisX(widht, height, borderPoints, minus)
+		if err != nil {
+			return path, err
+		}
+
+		if checkBorder(&path, auditory) {
+			return path, nil
+		} else {
+			return path, User000004
+		}
+	}
+}
+
+func drawAxisX(widht, height int, borderPoints *models.Reactangle, sign int) (models.Reactangle, error) {
 	var path models.Reactangle
 	var err error
 	switch sign {
@@ -76,7 +96,7 @@ func drawAxisX(borderPoints *models.Reactangle, sign int) (*models.Reactangle, e
 		err = User000004
 	}
 
-	return &path, err
+	return path, err
 }
 
 func defenitionAxis(position *models.Reactangle) int {
