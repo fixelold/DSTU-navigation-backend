98e6dcef74a028d4598c28ae3d7fc975caba2b50
Fixel <fixelti@gmail.com>
2023-03-28 19:11:40 +0300

    Create func min

--- internal/app/getPathPoints/pointsController.go
+++ internal/app/getPathPoints/pointsController.go
@@ -2,16 +2,22 @@ package getPathPoints
 
 import (
 	"navigation/internal/logging"
+	"navigation/internal/models"
 )
 
 type pointsController struct {
-	logger     *logging.Logger
-	repository Repository
+	logger     *logging.Logger // логирования.
+	repository Repository      // для обращения к базе данных.
+
+	audStart string               // номер начальной аудитории.
+	audEnd   string               // номер конечной аудитории.
+	sectors  []int                // массив номеров секторов
+	points   []models.Coordinates // массив координат. Для построения пути.
 }
 
 func NewPointsController(logger *logging.Logger, repository Repository) *pointsController {
 	return &pointsController{
-		logger: logger,
+		logger:     logger,
 		repository: repository,
 	}
 }
@@ -19,4 +25,14 @@ func NewPointsController(logger *logging.Logger, repository Repository) *pointsC
 func (p *pointsController) getPathPoints() error {
 
 	return nil
+}
+
+// TODO: добавить ошибки, если переменные одинаковые.
+func min(a, b int) (int, int, error) {
+	if a < b {
+		return a, b, nil
+	} else if a > b {
+		return b, a, nil
+	}
+	return a, b, nil
 }
