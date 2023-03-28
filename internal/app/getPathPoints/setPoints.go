d94bf91b7e600def7dae95df1e9783ff739fbcfa
Fixel <fixelti@gmail.com>
2023-03-28 20:00:14 +0300

    Added logic for set points data

--- internal/app/getPathPoints/setPoints.go
+++ internal/app/getPathPoints/setPoints.go
@@ -1,1 +1,52 @@
 package getPathPoints
+
+import "navigation/internal/models"
+
+func (d *data) setPoints(pointsType, axis, sign int, points models.Coordinates) (models.Coordinates, error) {
+	var path models.Coordinates
+	var err error
+
+	switch pointsType {
+	case audStartPoints:
+		switch axis {
+		case AxisX:
+			switch sign {
+			case plus:
+				path.X = points.X
+				path.Y = points.Y
+				path.Widht = points.Widht
+				path.Height = points.Height
+
+			case minus:
+				path.X = points.X
+				path.Y = points.Y
+				path.Widht = -points.Widht
+				path.Height = points.Height
+
+			default:
+				err = nil
+			}
+		case AxisY:
+			switch sign {
+			case plus:
+				path.X = points.X
+				path.Y = points.Y
+				path.Widht = points.Widht
+				path.Height = points.Height
+
+			case minus:
+				path.X = points.X
+				path.Y = points.Y
+				path.Widht = points.Widht
+				path.Height = -points.Height
+
+			default:
+				err = nil
+			}
+		}
+		return path, err
+
+	default:
+		return models.Coordinates{}, nil
+	}
+}
