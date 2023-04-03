ee2e7d177f97e38616a224b80e152ea1063282e4
Fixel <fixelti@gmail.com>
2023-04-03 18:23:27 +0300

    Refactor main.go file and moved the main logic

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,48 +1,10 @@
 package main
 
 import (
-	"context"
-	"navigation/internal/app/auditory"
-	"navigation/internal/app/getPathPoints"
-	"navigation/internal/app/pathBuilder"
-	"navigation/internal/app/user"
-	"navigation/internal/config"
-	"navigation/internal/database/client/postgresql"
-	"navigation/internal/logging"
-	"navigation/internal/transport/rest/middleware"
-
-	"github.com/gin-gonic/gin"
+	"navigation/cmd/app/startApp"
 )
 
 func main() {
-	appContext := context.Background()
-	logger := logging.GetLogger()
-	router := gin.Default()
-	v1Group := router.Group("/api/v1")
-
-	appConfig := config.GetConfig()
-	pgConn := postgresql.NewClient(appContext, *appConfig)
-
-	userPrepareRepo := user.NewRepository(pgConn, logger)
-	userPrepare := user.NewUser(logger, userPrepareRepo)
-	userPrepare.Create()
-
-	pathBuildingRepo := pathBuilder.NewRepository(pgConn, logger)
-	pathBuldingController := pathBuilder.NewHandler(logger, pathBuildingRepo)
-
-	drawPathRepo := getPathPoints.NewRepository(pgConn, logger)
-	drawPathController := getPathPoints.NewHandler(logger, drawPathRepo)
-
-	// users := user.NewRepository(pgConn, logger)
-	userController := user.NewHandler(logger, middleware.UserMiddleware{Client: pgConn, Logger: logger})
-
-	auditoryRepo := auditory.NewRepository(pgConn, logger)
-	auditoryController := auditory.NewHandler(logger, auditoryRepo, middleware.UserMiddleware{Client: pgConn, Logger: logger})
-
-	pathBuldingController.Register(v1Group)
-	drawPathController.Register(v1Group)
-	userController.Register(v1Group)
-	auditoryController.Register(v1Group)
-
-	logger.Fatalln(router.Run(":8080"))
+	var r startApp.Router
+	r.StartApp()
 }
---
+++ cmd/app/startApp/startApp.go
@@ -0,0 +1,59 @@
+package startApp
+
+import (
+	"context"
+	"navigation/internal/app/auditory"
+	"navigation/internal/app/getPathPoints"
+	"navigation/internal/app/pathBuilder"
+	"navigation/internal/app/user"
+	"navigation/internal/config"
+	"navigation/internal/database/client/postgresql"
+	"navigation/internal/logging"
+	"navigation/internal/transport/rest/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Router struct {
+	connection  postgresql.Client
+	logger      *logging.Logger
+	router      *gin.Engine
+	routerGroup *gin.RouterGroup
+}
+
+func (r *Router) StartApp() {
+	r.prepareData()
+	r.prepareRouter()
+	r.logger.Fatalln(r.router.Run(":8080"))
+}
+
+func (r *Router) prepareData() {
+	appContext := context.Background()
+	r.logger = logging.GetLogger()
+	r.router = gin.Default()
+	r.routerGroup = r.router.Group("/api/v1")
+	appConfig := config.GetConfig()
+	r.connection = postgresql.NewClient(appContext, *appConfig)
+}
+
+func (r *Router) prepareRouter() {
+	userPrepareRepo := user.NewRepository(r.connection, r.logger)
+	userPrepare := user.NewUser(r.logger, userPrepareRepo)
+	userPrepare.Create()
+
+	pathBuildingRepo := pathBuilder.NewRepository(r.connection, r.logger)
+	pathBuldingController := pathBuilder.NewHandler(r.logger, pathBuildingRepo)
+
+	drawPathRepo := getPathPoints.NewRepository(r.connection, r.logger)
+	drawPathController := getPathPoints.NewHandler(r.logger, drawPathRepo)
+
+	userController := user.NewHandler(r.logger, middleware.UserMiddleware{Client: r.connection, Logger: r.logger})
+
+	auditoryRepo := auditory.NewRepository(r.connection, r.logger)
+	auditoryController := auditory.NewHandler(r.logger, auditoryRepo, middleware.UserMiddleware{Client: r.connection, Logger: r.logger})
+
+	pathBuldingController.Register(r.routerGroup)
+	drawPathController.Register(r.routerGroup)
+	userController.Register(r.routerGroup)
+	auditoryController.Register(r.routerGroup)
+}
