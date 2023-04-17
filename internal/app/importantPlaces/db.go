3c05b1a4a19abec0995541dbe5f70fcb1e288e95
Fixel <fixelti@gmail.com>
2023-04-17 19:28:34 +0300

    base for database queries

--- internal/app/importantPlaces/db.go
+++ internal/app/importantPlaces/db.go
@@ -1,1 +1,49 @@
 package importantPlaces
+
+import (
+	"navigation/internal/appError"
+	"navigation/internal/database/client/postgresql"
+	"navigation/internal/logging"
+	"navigation/internal/models"
+)
+
+const (
+	file = "db.go"
+
+	createFunction 	= "create"
+	readFunction 	= "read"
+	updateFunction 	= "update"
+	deleteFunction 	= "delete"
+	listFunction 	= "list"
+)
+
+var (
+	txError 	= appError.NewAppError("can't start transaction")
+	queryError 	= appError.NewAppError("failed to complite the request")
+	scanError  	= appError.NewAppError("can't scan database response")
+)
+
+type repository struct {
+	client postgresql.Client
+	logger *logging.Logger
+}
+
+func newRepository(
+	client postgresql.Client, 
+	logger *logging.Logger,
+) Repository {
+	return &repository {
+		client: client,
+		logger: logger,
+	}
+}
+
+func (r *repository) Create(places models.ImportantPlaces) (models.ImportantPlaces, appError.AppError) {}
+
+func (r *repository) Read(id int) (models.ImportantPlaces, appError.AppError) {}
+
+func (r *repository) Update(oldpPlaces models.ImportantPlaces, newPlaces models.ImportantPlaces) (models.ImportantPlaces, appError.AppError) {}
+
+func (r *repository) Delete(id int) (appError.AppError) {}
+
+func (r *repository) List(numberBuild models.ImportantPlaces) ([]models.ImportantPlaces, appError.AppError) {}
--- internal/app/importantPlaces/storage.go
+++ internal/app/importantPlaces/storage.go
@@ -1,1 +1,14 @@
 package importantPlaces
+
+import (
+	"navigation/internal/appError"
+	"navigation/internal/models"
+)
+
+type Repository interface {
+	Create(places models.ImportantPlaces) (models.ImportantPlaces, appError.AppError)
+	Read(id int) (models.ImportantPlaces, appError.AppError)
+	Update(oldpPlaces models.ImportantPlaces, newPlaces models.ImportantPlaces) (models.ImportantPlaces, appError.AppError)
+	Delete(id int) (appError.AppError)
+	List(numberBuild models.ImportantPlaces) ([]models.ImportantPlaces, appError.AppError)
+}
